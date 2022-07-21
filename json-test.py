dab5e035c2efe88c0d0f52a8e0e4caa612ad1f81
Steven Coutts <stevec@couttsnet.com>
2022-07-21 15:01:12 +0100

    Read some json a different way

--- json-test.py
+++ json-test.py
@@ -1,6 +1,16 @@
 import json
+import os
+from pprint import pprint
 
-# Read the json file and output
-with open('example.json', 'r') as fcc_file:
-    fcc_data = json.load(fcc_file)
-    print(json.dumps(fcc_data, indent=4))
+# Get the absolute path for the directory where this file is located "here"
+here = os.path.abspath(os.path.dirname("example.json"))
+
+# Read in the JSON text
+with open(os.path.join(here, "example.json")) as file:
+    json_text = file.read()
+
+# Display the type of the json_text variable
+print("json_text is a", type(json_text))
+
+# Display the contents of the json_text variable
+print(json_text)
