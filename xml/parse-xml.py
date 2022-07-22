003201ffce818be2fab30196815b47445a98762d
Steven Coutts <stevec@couttsnet.com>
2022-07-22 15:40:22 +0100

    Tidy

--- xml/parse-xml.py
+++ xml/parse-xml.py
@@ -10,7 +10,6 @@ with open(os.path.join(here, "test.xml")) as file:
 
 # Convert the XML to a dict
 d = xmltodict.parse(xml_text)
-print(d)
 
 # Display the type of d
 print("d is a", type(d))
@@ -20,4 +19,4 @@ print(d["root"]["organization"])
 
 # Loop through switches and print hostname, model, and ip address
 for switch in d["root"]["switches"]:
- print(switch["hostname"] + "\t\t-\t\t" + switch["model"] + "\t\t-\t\t" + switch["ipaddress"])
+    print(switch["hostname"] + "\t\t-\t\t" + switch["model"] + "\t\t-\t\t" + switch["ipaddress"])
